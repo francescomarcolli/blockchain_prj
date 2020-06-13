3bc2fb210a6d6a75c68cf14213d8639945f36f6b
pyros <simonepirota@gmail.com>
2020-06-13 17:15:26 +0200

    Updated PayCoin

--- contracts/PayCoin.sol
+++ contracts/PayCoin.sol
@@ -10,18 +10,22 @@ import "OpenZeppelin/openzeppelin-contracts@2.5.0/contracts/token/ERC20/ERC20Det
 contract PayCoin is Context, IT_PayCoin, ERC20Detailed, ERC20Burnable, BurnerRole{
 
     address _superUser = 0x85A8d7241Ffffee7290501473A9B11BFdA2Ae9Ff ;
-    address _exchange = 0x5831dE8826c01CC7fB165b30D59356D9BA27886F ;
+    //address _exchange = 0x5831dE8826c01CC7fB165b30D59356D9BA27886F ;
 
     //Constructor meant to be called on deploy
     constructor () public ERC20Detailed("PayCoin", "PaC", 18){
+        
         if (!(isMinter(_superUser) && isBurner(_superUser))){
             addMinter(_superUser);
             addBurner(_superUser);
         }
+        
+        /*
         if (!(isMinter(_exchange) && isBurner(_exchange))){
             addMinter(_exchange);
             addBurner(_exchange);
         }
+        */
     }
 
     function burn(uint256 amount) public onlyBurner {
