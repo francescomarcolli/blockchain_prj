550af647b26c8e13b9f45147c79a52e2783ee0e8
pyros <simonepirota@gmail.com>
2020-06-20 21:22:59 +0200

    Now it's working, just have to specify what to do for each different event

--- pyscripts/challenge_scripts/monitorRopsten.py
+++ pyscripts/challenge_scripts/monitorRopsten.py
@@ -1,115 +1,70 @@
 from brownie import web3, network, Wei, Contract, project
 from brownie.network.account import LocalAccount
 #from web3 import Web3
-import json, time
-from web3.contract import ContractEvents
-from web3.contract import ContractEvent
-from web3 import Web3
+import json, time, sys
+#from web3.contract import ContractEvents
+#from web3.contract import ContractEvent
+#from web3 import Web3
 
+def monitorContract(web3Contract, blockNumber):            
+    if(web3.eth.getBlock('latest')['number'] != blockNumber):
+        all_events = web3.eth.getLogs({'fromBlock': blockNumber, 'toBlock': 'latest', 'address': web3Contract.address})
 
-<<<<<<< HEAD
+        for event_data in all_events: 
+            tx_hash = event_data['transactionHash'].hex()
+            receipt = web3.eth.getTransactionReceipt(tx_hash)
+            events = [event['name'] for event in web3Contract.events._events]
+            logs = [ web3Contract.events.__dict__[event_name]().processReceipt(receipt) for event_name in events ]
+            print(logs)
+            #logs = [ contract.events.OpenLoan().processReceipt(receipt) for event_name in events ]
+
+network_selected = "ropsten"
+network.connect(network_selected)
+
 # Loading the metamask account
 fss_private_key = "E45161BD0BACE1E6F28B28BF49A96A5F4D81D133D09A6E3E18674422D9FD47C4"
 fss_account = web3.eth.account.from_key(private_key=fss_private_key)
 local_account = LocalAccount(fss_account.address, fss_account, fss_account.privateKey)
 
-# Opening contracts
-with open("path_to_abi") as json_file: 
+# checking if the script is called in the right way
+if (len(sys.argv) < 2):
+    print('Usage: {} <address> <path_to_abi_file> [poll_interval]'.format(sys.argv[0]))
+    exit()
+
+#read arguments
+[address, abi_file] = sys.argv[1:3]
+poll_interval = 5
+if (len(sys.argv) > 3):
+    poll_interval = int(sys.argv[3]) 
+
+# Opening the payCoin contract
+with open("./pyscripts/paycoin_abi.json") as json_file: 
     abi_pc = json.load(json_file)
-payCoin = Contract.from_abi('', address=, abi=abi_pc, owner= local_account)
+payCoin = Contract.from_abi('PayCoin', address="0x7F7A26538Cb8A2cc93229B782D5b716b47c118CD", abi=abi_pc, owner= local_account)
 
-
-def monitorContract(self, contract, blockNumber):            
-    if(web3.eth.getBlock('latest')['name'] != blockNumber):
-=======
-
-
-
-def monitorContract(contract, blockNumber):            
-    if(web3.eth.getBlock('latest') != blockNumber):
->>>>>>> 84e8655d2a9b3e1f9be4b04f1f45245333cc0636
-        all_events = web3.eth.getLogs({'fromBlock': blockNumber, 'toBlock': 'latest', 'address': contract.address})
-
-        for event_data in all_events: 
-            tx_hash = event_data['transactionHash'].hex()
-            receipt = web3.eth.getTransactionReceipt(tx_hash)
-            #events = [event for event in contract.topics.keys()]
-            #logs = contract.events.OpenLoan().processReceipt(receipt)  ### CODE BREAKING
-            events = [event for event in contract.events._events]
-           # logs = [ contract.events.__dict__[event_name]().processReceipt(receipt) for event_name in events ]
-            logs = [ contract.events.OpenLoan().processReceipt(receipt) for event_name in events ]
-
-
-def main():
-<<<<<<< HEAD
-    if (len(sys.argv) < 2):
-        print('Usage: {} <address> <path_to_abi_file> [poll_interval]'.format(sys.argv[0]))
-        return
-    #read arguments
-    [address, abi_file] = sys.argv[1:3]
-    poll_interval = 5
-    if (len(sys.argv) > 3):
-        poll_interval = int(sys.argv[3]) 
+# opening the contract to monitor in web3 and brownie format
+with open(abi_file) as json_file:
+    abi_contract = json.load(json_file)
+# create the web3 contract object   
+web3Contract = web3.eth.contract(abi=abi_contract, address=address) #create the contract object
+# create the brownie contract object
+brownieContract = Contract.from_abi('bContract', address=address, abi=abi_contract, owner= local_account)
     
-    #load and parse abi file
-    with open(abi_file) as f:
-        abi_data = json.load(f)
-        filterContract = w3.eth.contract(abi=abi_data, address=address) #create the contract object
-        #filter = w3.eth.filter({'fromBlock': 'latest', 'toBlock': 'latest', 'address': address}) #create the filter
-        monitorContract(filterContract, startBlock, poll_interval) #start polling for new events
-    
-if __name__ == '__main__':
-    main()
-=======
+# saving the latest block number
+startBlock = web3.eth.blockNumber 
 
-    # Connecting to the network
-    network_selected = "ropsten"
-    network.connect(network_selected)
+print('Listening to transactions...')
 
-# Loading the metamask account
-    fss_private_key = "E45161BD0BACE1E6F28B28BF49A96A5F4D81D133D09A6E3E18674422D9FD47C4"
-    fss_account = web3.eth.account.from_key(private_key=fss_private_key)
-    local_account = LocalAccount(fss_account.address, fss_account, fss_account.privateKey)
->>>>>>> 84e8655d2a9b3e1f9be4b04f1f45245333cc0636
-
-
-    [address, abi_file] = ['0xa4f9c9080e2Ed5e361b764A67023CC98d706cF38','/home/simone/Dropbox/Universita/MCF/blockchain_course/blockchain_course_unimi/challenge/teamCST/abi/Lender.json']
-
-    with open(abi_file) as f:
-        abi_data = json.load(f)
-
-    CST_Lender= Contract.from_abi('filterContract',abi=abi_data, address=address, owner=local_account)
-    CST_LenderW3 = web3.eth.contract(abi=abi_data, address=address)  
-# Opening contracts
-    #with open("path_to_abi") as json_file: 
-     #   abi_pc = json.load(json_file)
-    #payCoin = Contract.from_abi('', address=, abi=abi_pc, owner= local_account)
-
-    #with open("path_to_abi") as json_file: 
-     #   abi_T1_Challenge = json.load(json_file)
-    #T1_Challenge = Contract.from_abi('', address=, abi=abi_T1_Challenge, owner= local_account)
-
-    #with open("path_to_abi") as json_file: 
-     #   abi_T3_Challenge = json.load(json_file)
-    #T3_Challenge = Contract.from_abi('', address=, abi=abi_T3_Challenge, owner= local_account)
-
-    #with open("path_to_abi") as json_file: 
-     #   abi_my_Challenge = json.load(json_file)
-    #my_Challenge = Contract.from_abi('myCHallenge', address=, abi=abi_my_Challenge, owner= local_account)
-    
-    #my_provider = Web3.HTTPProvider('https://ropsten.infura.io/v3/0a4abfedee2e4ca68379cf779a938815')
-    #w3 = Web3(my_provider)
-
-
-    startBlock = web3.eth.blockNumber 
+# start monitoring the contract on the blockchain
+while True: 
+    monitorContract(web3Contract, startBlock)
+    startBlock = web3.eth.blockNumber
+    time.sleep(poll_interval)
     print('Listening to transactions...')
-    while True: 
-        monitorContract(CST_LenderW3, startBlock)
-        #monitorContract(contract=T3_Challenge, startBlock)
-        startBlock = web3.eth.blockNumber
-        time.sleep(5)
-        print('Listening to transactions...')
+    
 
+#[address, abi_file] = ['0xa4f9c9080e2Ed5e361b764A67023CC98d706cF38',
+#'/home/simone/Dropbox/Universita/MCF/blockchain_course/blockchain_course_unimi/challenge/teamCST/abi/Lender.json']
+#CST_Lender= Contract.from_abi('filterContract',abi=abi_data, address=address, owner=local_account)
+#CST_LenderW3 = web3.eth.contract(abi=abi_data, address=address)  
 
-if __name__ == '__main__':
-    main()
